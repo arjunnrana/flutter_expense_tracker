5e7a1a8623adc78ec07e45fc102db203f6e65523
Arjun Rana <gemthearjun@gmail.com>
2023-05-25 12:28:09 +0530

    Using Theme Data in Widgets

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -25,9 +25,9 @@ void main() {
         ),
         textTheme: ThemeData().textTheme.copyWith(
               titleLarge: TextStyle(
-                fontWeight: FontWeight.normal,
-                color: kColorScheme.secondaryContainer,
-                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                color: kColorScheme.onSecondaryContainer,
+                fontSize: 16,
               ),
             ),
       ),
